25c67c7eebe40864ebb0107a97079f0101070e96
Haard Solanki <haardsolanki.itm@gmail.com>
2024-10-01 20:07:14 +0530

    deployement left

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -6,9 +6,43 @@ import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
 import {MessageHashUtils} from '@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol';
 import {ECDSA} from '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
 import {SIG_VALIDATION_FAILED,SIG_VALIDATION_SUCCESS } from 'lib/account-abstraction/contracts/core/Helpers.sol';
+import {IEntryPoint } from 'lib/account-abstraction/contracts/interfaces/IEntryPoint.sol';
 contract MinimalAccount is IAccount,Ownable{
-    constructor() Ownable(msg.sender){}
+    error MinimalAccount_notFromEntryPoint();
+    error MinimalAccount_notFromEntryPointorOwner();
+    error MinimalAccount_CallFailed(bytes );
+    IEntryPoint private immutable  i_entryPoint;
+    modifier requireFromEntryPoint(){
+        if(msg.sender!=address(i_entryPoint)){
+            // revert("MinimalAccount: Only entrypoint can call this function");
+            revert MinimalAccount_notFromEntryPoint();
+        }
+        _;
+    }
+    modifier requireFromEntryPointorOwner(){
+        if(msg.sender!=address(i_entryPoint) && msg.sender!=owner()){
+            // revert("MinimalAccount: Only entrypoint can call this function");
+            revert MinimalAccount_notFromEntryPointorOwner();
+        }
+        _;
+    }
+    constructor(address entrypoint) Ownable(msg.sender){
+        i_entryPoint=IEntryPoint(entrypoint);
+    }
 
+
+
+    // recieve()
+    ////////////////////////////
+    //  External functions
+    ////////////////////////////
+    function execute(address dest,uint256 value, bytes calldata functionData) external requireFromEntryPointorOwner{
+        (bool success,bytes memory result)=dest.call{value:value}(functionData);
+        if(!success){
+            revert MinimalAccount_CallFailed(result);
+
+        }
+    }
     //A signature is valid if it is the MinimalAccount Owner
     function validateUserOp(
         PackedUserOperation calldata userOp,
@@ -18,8 +52,19 @@ contract MinimalAccount is IAccount,Ownable{
     {
         //validating userop
        validationData= _validateSignature(userOp,userOpHash);
+       _payPrefund(missingAccountFunds);
        return validationData;
 
+    }
+    ////////////////////////////
+    //  Internal functions
+    ////////////////////////////
+    function _payPrefund(uint256 missingAccountFunds) internal{
+        if(missingAccountFunds!=0){
+            (bool success,)=payable(msg.sender).call{value:missingAccountFunds,gas: type(uint256).max}("");
+            (success);
+            
+        }
     }
     function _validateSignature(PackedUserOperation calldata userOp,bytes32 userOpHash) internal view returns(uint256 validationData){
         bytes32 ethSignedMessageHash=MessageHashUtils.toEthSignedMessageHash(userOpHash);
@@ -29,4 +74,11 @@ contract MinimalAccount is IAccount,Ownable{
         }
         return SIG_VALIDATION_SUCCESS;
     }
+    ////////////////////////////
+    //  Getter functions
+    ////////////////////////////
+    function getEntryPoint() external view returns (address) {
+        return address(i_entryPoint);
+    }   
+
 }
